3e17be307da9b2f85a184fa869da423061fbfcbc
Tonio1999 <44210786+Tonio1999@users.noreply.github.com>
2019-11-22 18:12:43 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,70 +1,60 @@
-/* The main test file for the Operating Systems C-Linked List for storing strings*/
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "list.h"
-
-
 int main() 
 {
-
-    /*Testing creation and addition.*/
-    printf("**********************************************************\n");
-    printf("Testing the creation and the addition of elements to the list.\n\n");
-    list* test1;
-    test1 = create_list();
-
-    printf("**********************************************************\n");
-    printf("List created. Adding the strings: John, Hi, Martin, Ricky.\n\n");
-    add_to_list(test1, "Barry");
-    add_to_list(test1, "Jerry");
-    add_to_list(test1, "Walter");
-    add_to_list(test1, "Thomas");
-
-    printf("**********************************************************\n");
-    printf("Elements added to the list. Now Printing the list.\n\n");
-    print_list(test1);
-    printf("\n");
-
-    /* Testing the removal */
-    printf("**********************************************************\n");
-    printf("Call the remove function. Should remove the 1st element of the list.\n\n");
-    char* deleted_str;
-    deleted_str = remove_from_list(test1);
-
-    printf("**********************************************************\n");
-    printf("Element Removed. Now printing the updated list and deleted node.\n\n");
-    print_list(test1);
-    printf("\nDeleted String: %s\n\n", deleted_str);
-
-    /* Second removal. */
-    printf("**********************************************************\n");
-    printf("Called the remove function. Should remove the 1st element of the list.\n\n");
-    deleted_str = remove_from_list(test1);
-
-    printf("**********************************************************\n");
-    printf("Element Removed. Now printing the updated list and deleted node.\n\n");
-    print_list(test1);
-    printf("\nDeleted String: %s\n\n", deleted_str);
-
-    /* Testing the flush of list function*/
-    printf("**********************************************************\n");
-    printf("Called the flush function. Should create an empty list.\n");
-    flush_list(test1);
-    if ((test1->head == NULL) && (test1->tail == NULL)) 
+	printf("Tests for linked list implementation.\n");
+    printf("We assume index starts from 0.\n\n");
+	list_other *list1 = list_alloc();
+	list_add_to_front(list1,2);
+    printf("Add %d to the front: ", 2);
+    list_print(list1);
+	list_add_to_front(list1,1);
+    printf("Add %d to the front: ", 1);
+    list_print(list1);
+    list_add_to_back(list1,3);
+    printf("Add %d to the back: ", 3);
+    list_print(list1);
+	list_add_to_back(list1,4);
+    printf("Add %d to the back: ", 4);
+    list_print(list1);
+	list_add_at_index(list1,7,3);
+    printf("Add %d at index %d: ", 7,3);
+    list_print(list1);
+	list_add_to_back(list1,5);
+    printf("Add %d to the back: ", 5);
+    list_print(list1);
+	list_add_to_front(list1,6);
+    printf("Add %d to the front: ", 6);
+    list_print(list1);
+	list_add_at_index(list1,9,5);
+    printf("Add %d at index %d: ", 9,5);
+    list_print(list1);
+	elem e1 = list_remove_from_front(list1);
+    printf("\nRemoved %d from the front: ", e1);
+    list_print(list1);
+	elem e2 = list_remove_at_index(list1, 3);
+    printf("Removed %d from the index, %d: ", e2, 3);
+    list_print(list1);
+	elem e3 = list_remove_from_back(list1);
+    printf("Removed %d from the back: ", e3);
+	list_print(list1);
+	int length = list_length(list1);
+	printf("\nLength of the list is: %d.\n", length);
+	elem item = 10;
+	bool exist = list_is_in(list1, item);
+	if (exist)
     {
-        printf("The list flush was successful.\n\n");
-    }
-
-    /* Testing the free list function*/
-    printf("**********************************************************\n");
-    printf("Called the free list. Should the list and everything in it.\n\n");
-    free_list(test1);
-
-    printf("Program Testing has ended. Hope you enjoyed. :) \n");
-
-
-    return 0;
-
+		printf("%d is in the list.\n", item);
+	} else 
+    {
+		printf("%d is not in the list.\n", item);
+	}
+	int index1 = 2;
+	elem e4 = list_get_elem_at(list1, index1);
+	printf("Element at index %d is: %d. \n", index1, e4);
+	elem e5 = 2;
+	int index2 = list_get_index_of(list1,e5);
+	printf("Element %d is at index: %d. \n", e5, index2);
+	list_free(list1);
+	return 0;
 }
